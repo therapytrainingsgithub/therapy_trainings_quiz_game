87bc75f490ac96fca7427c23fefcd237ee193ff3
therapytrainingsgithub <support@therapytrainings.com>
2024-10-03 13:47:28 +0500

    AFEW

--- next.config.js
+++ next.config.js
@@ -13,15 +13,17 @@ const nextConfig = {
   },
   async headers() {
     return [
-        {
-            source: '/:path*',
-            headers: [
-                { key: 'Content-Security-Policy', value: "frame-ancestors 'self' https://therapy-trainings-quiz-game-psi.vercel.app/quiz;" },
-                // Allow iFrame embedding from your domain and the domain you want
-            ],
-        },
-    ];
-}
+      {
+        source: "/:path*",
+        headers: [
+          { key: "Access-Control-Allow-Origin", value: "*" },
+          { key: "Access-Control-Allow-Methods", value: "GET, POST, OPTIONS, PUT, PATCH, DELETE" },
+          { key: "Access-Control-Allow-Credentials", value: "true" },
+          { key: "Access-Control-Allow-Headers", value: "Authorization, Content-Type" },
+        ],
+      },
+    ]
+  },
 };
 
 module.exports = nextConfig;
