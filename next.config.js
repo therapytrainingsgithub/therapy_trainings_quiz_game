71fadcbdd01058f53775f466def6cdef383d31a9
therapytrainingsgithub <support@therapytrainings.com>
2024-10-03 12:12:36 +0500

    AFEW

--- next.config.js
+++ next.config.js
@@ -18,28 +18,17 @@ const nextConfig = {
         headers: [
           {
             key: 'Content-Security-Policy',
-            value: "frame-ancestors 'self' https://therapy-trainings-quiz-game-psi.vercel.app/quiz;"
+            value: "frame-ancestors 'self' *;"
           },
           {
             key: 'X-Frame-Options',
-            value: 'ALLOW-FROM https://therapy-trainings-quiz-game-psi.vercel.app/quiz'
-          },
-          {
-            key: 'Cache-Control',
-            value: 'no-cache, no-store, must-revalidate'
-          },
-          {
-            key: 'Pragma',
-            value: 'no-cache'
-          },
-          {
-            key: 'Expires',
-            value: '0'
+            value: 'ALLOWALL' // Allows your app to be embedded in an iframe
           }
         ]
       }
     ];
   }
+  
 };
 
 module.exports = nextConfig;
